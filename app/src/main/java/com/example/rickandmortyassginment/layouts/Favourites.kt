b8149f05b46f84c44775b18d0ee5f670ccee528f
unknown <jfjoao12@gmail.com>
2025-02-26 12:07:20 -0600

    Prepared for next assignment

---
+++ app/src/main/java/com/example/rickandmortyassginment/layouts/Favourites.kt
@@ -0,0 +1,10 @@
+package com.example.rickandmortyassginment.layouts
+
+import androidx.compose.runtime.Composable
+import com.example.rickandmortyassginment.api.models.CharactersManager
+
+@Composable
+fun ShowListOfFavourites(card: CharactersManager) {
+    // do code here
+    // maybe dont even need CharactersManager
+}
